582e9d76821797bd9ae53d2a37eb07ad942a42b5
markdee <markmudyiwah0@gmail.com>
2024-11-04 20:04:33 +0200

    create the product js file

---
+++ scripts/products.js
@@ -0,0 +1,165 @@
+const products = [
+
+{
+    id:123,
+    image:"images/product-images/gourmet-burger.jpg",
+    name:'Gourmet Burger',
+    price:50,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:456,
+    image:"images/product-images/Everyday-burger.jpg",
+    name:'Everyday Burger',
+    price:45,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:789,
+    image:"images/product-images/chicken-burger.jpg",
+    name:'Chicken Burger',
+    price:45,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:1011,
+    image:"images/product-images/Hot-dog.jpg",
+    name:'Hot Dog',
+    price:59,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:1213,
+    image:"images/product-images/sausage-rolls.jpeg",
+    name:'Sausage Rolls',
+    price:30,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:1415,
+    image:"images/product-images/breakfast-toastie",
+    name:'Breakfast Toastie',
+    price:33,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:1617,
+    image:"images/product-images/chip-roll2.jpg",
+    name:'Chip Roll',
+    price:20,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:1819,
+    image:"images/product-images/chicken-mayo-toastie.jpg",
+    name:'Chicken Mayo toastie',
+    price:33,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:1920,
+    image:"images/product-images/milkshakes.jpg",
+    name:'Milk Shakes',
+    price:35,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:2021,
+    image:"images/product-images/switch.jpg",
+    name: 'Switch Energy Drink',
+    price:10.99,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:2122,
+    image:"images/product-images/score-drink.jpg",
+    name:'Score Energy',
+    price:10.99,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:2223,
+    image:"images/product-images/double-o-drinks.png",
+    name:'  Double "O"',
+    price:14.99,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:2324,
+    image:"images/product-images/",
+    name:Burger,
+    price:59,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:2425,
+    image:"images/product-images/",
+    name:Burger,
+    price:59,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:2526,
+    image:"images/product-images/",
+    name:Burger,
+    price:59,
+    info:"one of the best stuff"
+}
+,
+
+
+{
+    id:2627,
+    image:"images/product-images/",
+    name:Burger,
+    price:59,
+    info:"one of the best stuff"
+}
+,
+
+
+
+
+]
